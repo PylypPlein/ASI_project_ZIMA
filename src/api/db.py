6b5182a22c7757dac691c738a3c8b8e5167668cf
kaczorel <wiktor.kaczor@gmail.com>
2025-12-09 20:48:31 +0100

    tests fixed

--- src/api/db.py
+++ src/api/db.py
@@ -5,11 +5,14 @@ from src.api.config import settings
 
 engine = create_engine(settings.DATABASE_URL, future=True)
 
+
 def save_prediction(payload: dict, prediction: float, model_version: str):
     with engine.begin() as conn:
 
         if engine.url.get_backend_name() == "sqlite":
-            conn.execute(text("""
+            conn.execute(
+                text(
+                    """
             CREATE TABLE IF NOT EXISTS predictions(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 ts TEXT,
@@ -17,9 +20,13 @@ def save_prediction(payload: dict, prediction: float, model_version: str):
                 prediction REAL,
                 model_version TEXT
             )
-            """))
+            """
+                )
+            )
         else:
-            conn.execute(text("""
+            conn.execute(
+                text(
+                    """
             CREATE TABLE IF NOT EXISTS predictions(
                 id SERIAL PRIMARY KEY,
                 ts TIMESTAMP,
@@ -27,14 +34,21 @@ def save_prediction(payload: dict, prediction: float, model_version: str):
                 prediction DOUBLE PRECISION,
                 model_version TEXT
             )
-            """))
+            """
+                )
+            )
 
-        conn.execute(text("""
+        conn.execute(
+            text(
+                """
         INSERT INTO predictions(ts, payload, prediction, model_version)
         VALUES (:ts, :payload, :pred, :ver)
-        """), {
-            "ts": dt.datetime.utcnow().isoformat(),
-            "payload": json.dumps(payload),
-            "pred": float(prediction),
-            "ver": model_version
-        })
+        """
+            ),
+            {
+                "ts": dt.datetime.utcnow().isoformat(),
+                "payload": json.dumps(payload),
+                "pred": float(prediction),
+                "ver": model_version,
+            },
+        )
